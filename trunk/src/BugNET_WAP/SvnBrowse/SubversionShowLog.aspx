8bf63f136ed3273f77fb7f998185af3b1eee26e4
apdyx168@gmail.com <apdyx168@gmail.com@4d642473-e215-c9e0-d129-36c6e9c1963a>
2011-12-06 06:31:02 +0000

    修改程序以便读取subversion日志信息

--- trunk/src/BugNET_WAP/SvnBrowse/SubversionShowLog.aspx
+++ trunk/src/BugNET_WAP/SvnBrowse/SubversionShowLog.aspx
@@ -1,18 +1,35 @@
 ﻿<%@ Page Language="C#" Title="ShowLog" MasterPageFile="~/Shared/SingleColumn.master" AutoEventWireup="true" CodeBehind="SubversionShowLog.aspx.cs" Inherits="BugNET.SvnBrowse.SubversionShowLog" %>
 
  <asp:Content ContentPlaceHolderID="PageTitle" runat="server" ID="content5">
- <asp:GridView ID="SvnLog" runat="server" 
+ 
+  </asp:Content>
+
+  <asp:Content ContentPlaceHolderID="Content" runat="server" ID="content1">
+      <asp:GridView ID="SvnLog" runat="server" 
     AutoGenerateColumns="False">
     <Columns>      
-        <asp:BoundField DataField="Rev" 
-            HeaderText="Rev" ReadOnly="True" />
+        <asp:BoundField DataField="Rev"  
+            HeaderText="Rev" ReadOnly="True" >
+        <ControlStyle Width="100px" />
+        <HeaderStyle Width="100px" />
+        </asp:BoundField>
         <asp:BoundField DataField="Log" 
             HeaderText="Message" InsertVisible="False"
-            ReadOnly="True" />
+            ReadOnly="True" > 
+         <ControlStyle Width="1200px" />
+        <HeaderStyle Width="1200px" />
+            </asp:BoundField>
         <asp:BoundField DataField="Date" 
-            HeaderText="Date"  />
+            HeaderText="Date"  >
+            <ControlStyle Width="100px" />
+        <HeaderStyle Width="100px" />
+            </asp:BoundField>
         <asp:BoundField DataField="User" 
-            HeaderText="User" />      
+            HeaderText="User" >    
+            <ControlStyle Width="100px" />
+        <HeaderStyle Width="100px" />
+            </asp:BoundField>  
     </Columns>
 </asp:GridView>
-  </asp:Content>
+</asp:Content>
+
--- trunk/src/BugNET_WAP/SvnBrowse/SubversionShowLog.aspx.cs
+++ trunk/src/BugNET_WAP/SvnBrowse/SubversionShowLog.aspx.cs
@@ -5,58 +5,216 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace BugNET.SvnBrowse
 {
-    public partial class SubversionShowLog : System.Web.UI.Page
+    public partial class SubversionShowLog : BugNET.UserInterfaceLayer.BasePage
     {
+        private static Dictionary<int, string> errors = new Dictionary<int, string>();
+      
+        DataTable dt ;       
+        private string  singleLog = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            DataTable dt = new DataTable("Log");
-            DataColumn column;
+            if (!IsPostBack)
+            {                
+                DataColumn column;
+                string svnUrl = "https://192.168.0.225/svn/openflow/program/trunk";
 
-            column = new DataColumn();
-            column.DataType = System.Type.GetType("System.String");
-            column.ColumnName = "Rev";
-            column.ReadOnly = true;
-            column.Unique = true;
-            dt.Columns.Add(column);
+                dt = new DataTable("Log");
 
-            column = new DataColumn();
-            column.DataType = System.Type.GetType("System.String");
-            column.ColumnName = "Log";
-            column.ReadOnly = true;
-            column.Unique = true;
-            dt.Columns.Add(column);
+                column = new DataColumn();
+                column.DataType = System.Type.GetType("System.String");
+                column.ColumnName = "Rev";
+                column.ReadOnly = true;
+                dt.Columns.Add(column);
 
-            column = new DataColumn();
-            column.DataType = System.Type.GetType("System.String");
-            column.ColumnName = "Date";
-            column.ReadOnly = true;
-            column.Unique = true;
-            dt.Columns.Add(column);
+                column = new DataColumn();
+                column.DataType = System.Type.GetType("System.String");
+                column.ColumnName = "Log";
+                column.ReadOnly = true;
 
-            column = new DataColumn();
-            column.DataType = System.Type.GetType("System.String");
-            column.ColumnName = "User";
-            column.ReadOnly = true;
-            column.Unique = true;
-            dt.Columns.Add(column);
+                dt.Columns.Add(column);
 
-            DataSet ds = new DataSet();
-            ds.Tables.Add(dt);
+                column = new DataColumn();
+                column.DataType = System.Type.GetType("System.DateTime");
+                column.ColumnName = "Date";
+                column.ReadOnly = true;
 
-            DataRow dr = dt.NewRow();
+                dt.Columns.Add(column);
 
-            dr["rev"] = "T";
-            dr["Log"] = "E";
-            dr["Date"] = "S";
-            dr["User"] = "T";
+                column = new DataColumn();
+                column.DataType = System.Type.GetType("System.String");
+                column.ColumnName = "User";
+                column.ReadOnly = true;
 
-            dt.Rows.Add(dr);
+                dt.Columns.Add(column);
 
-            SvnLog.DataSource = dt;
-            SvnLog.DataBind();
+                SvnReadLog(svnUrl);
+                
+                SvnLog.DataSource = dt;
+                SvnLog.DataBind();
+            }
         }
+
+        protected void SvnReadLog(string svnUrl)
+        {
+            string command = "svn";
+            string args = string.Format("--username {0} --password {1} --non-interactive --trust-server-cert log {2}", "dyx", "holystar", svnUrl);
+            //string command = "cmd";
+            //string args = "dir";          
+
+            Process proc = null;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(command, args);
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            //startInfo.RedirectStandardError = true;
+            
+
+            try
+            {
+                proc = new Process();
+                proc.StartInfo = startInfo;
+                proc.OutputDataReceived += new DataReceivedEventHandler(proc_OutputDataReceived);
+                proc.Start();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(ex.Message);
+            }
+
+            proc.BeginOutputReadLine();   
+
+            proc.WaitForExit();
+        }
+
+        void proc_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                string s = e.Data;
+
+                if (s.IndexOf("------") == 0)
+                {
+                    if (string.IsNullOrEmpty(singleLog)) return;
+                    var lines = singleLog.Split(new char[]{'|','\r'});
+
+                    if (lines.Length == 5)
+                    {
+                        DataRow dr = dt.NewRow();
+                        dr["rev"] = lines[0];
+                        //dr["date"] = DateTime.Parse(lines[1].Substring(0,lines[1].IndexOf('(') - 1));
+                        dr["user"] = lines[1];
+                        dr["date"] = DateTime.Parse(lines[2].Substring(0, lines[2].IndexOf('(') - 1));
+                        dr["log"] = lines[4];
+
+                        dt.Rows.Add(dr);
+                    }
+                    singleLog = "";
+                }
+                else
+                {
+                    char t = ' ';
+                    if (singleLog =="") t = '|';
+                    
+                    singleLog += e.Data + t;
+                }
+
+            }
+        }
+
+        public static string RunCommand(string command, string args, int killAfterSeconds, bool echoCommand)
+        {
+            Process proc = null;
+            log4net.ILog logger = log4net.LogManager.GetLogger("CommandExecutor");
+
+            if (logger.IsDebugEnabled) logger.DebugFormat("Running Commandline: {0} {1}", command, args);
+
+            try
+            {
+                var startInfo = new ProcessStartInfo(command, args);
+                startInfo.CreateNoWindow = true;
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.RedirectStandardError = true;
+
+                
+                proc = new Process();
+                proc.StartInfo = startInfo;
+                proc.ErrorDataReceived += new DataReceivedEventHandler(CommandProcess_ErrorDataReceived);
+                proc.Start();
+
+                proc.BeginErrorReadLine();
+
+                string retVal = proc.StandardOutput.ReadToEnd();                
+
+                if (!proc.WaitForExit(killAfterSeconds * 1000))
+                    proc.Kill();
+
+                if (errors.ContainsKey(proc.Id))
+                    retVal += Environment.NewLine + "Error: " + Environment.NewLine + errors[proc.Id];
+
+                if (echoCommand)
+                {
+                    // hide password from being displayed
+                    Regex RegexObj = new Regex("--password\\s+\\S+\\s", RegexOptions.IgnoreCase);
+                    args = RegexObj.Replace(args, "--password **** ");
+
+
+                    return command + " " + args + Environment.NewLine + retVal;
+                }
+                else
+                {
+                    return retVal;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorFormat("An error occurred running the command line: {2} {3}\n\n {0} \n\n {1}", ex.Message, ex.StackTrace, command, args);
+                return string.Empty;
+            }
+            finally
+            {
+                if (proc != null)
+                {
+                    if (errors.ContainsKey(proc.Id))
+                        errors.Remove(proc.Id);
+
+                    proc.Dispose();
+                }
+            }
+
+        }
+
+        static void CommandProcess_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            // RC: Sometimes an error occurres in here. I think the process is ending while we are getting the data, but Im not sure.
+            // I'm stuffing it for now.
+            try
+            {
+                if (sender != null)
+                {
+                    if (e.Data != null && e.Data.Length > 0)
+                    {
+                        int id = ((Process)sender).Id;
+
+                        if (errors.ContainsKey(id))
+                            errors[id] += Environment.NewLine + e.Data;
+                        else
+                            errors.Add(id, e.Data);
+                    }
+                }
+            }
+            catch { }
+        }
+
     }
 }
